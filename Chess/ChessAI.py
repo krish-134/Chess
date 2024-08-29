9a89b02545ae04347f64e32d446b1ef0209cd579
krish-134 <garchakri@gmail.com>
2024-08-28 21:00:25 -0700

    Removed commented out method in ChessAI.py

--- Chess/ChessAI.py
+++ Chess/ChessAI.py
@@ -103,50 +103,6 @@ def find_best_move_no_recursion(gs, valid_moves):
         gs.undo_move()
     return best_player_move
 
-
-    # turn_multiplier = 1 if gs.white_to_move else -1
-    # opponent_min_max_score = CHECKMATE
-    # best_player_move = None
-    # random.shuffle(valid_moves)
-    # for player_move in valid_moves:
-    #     gs.make_move(player_move)
-    #     opponents_moves = gs.get_valid_moves()
-    #     if gs.stalemate:
-    #         opponent_max_score = STALEMATE
-    #     elif gs.checkmate:
-    #         opponent_max_score = -CHECKMATE
-    #     else:
-    #         opponent_max_score = -CHECKMATE
-    #         for opponent_move in opponents_moves:
-    #             gs.make_move(opponent_move)
-    #             gs.get_valid_moves()
-    #             if gs.checkmate:
-    #                 score = CHECKMATE
-    #             elif gs.stalemate:
-    #                 score = STALEMATE
-    #             else:
-    #                 score = -turn_multiplier * score_material(gs.board)
-    #             if score > opponent_max_score:
-    #                 opponent_max_score = score
-    #             gs.undo_move()
-    #     if opponent_max_score < opponent_min_max_score:
-    #         opponent_min_max_score = opponent_max_score
-    #         best_player_move = player_move
-    #     gs.undo_move()
-    # return best_player_move
-
-# def find_best_move(gs, valid_moves, return_queue):
-#     global next_move, counter
-#     counter = 0
-#     next_move = None
-#     #find_move_min_max(gs, valid_moves, DEPTH, gs.whiteToMove)
-#     #find_move_nega_max(gs, valid_moves, DEPTH, 1 if gs.whiteToMove else -1)
-#     #find_move_nega_max_alpha_beta(gs, valid_moves, DEPTH, -CHECKMATE, CHECKMATE, 1 if gs.white_to_move else -1)
-#     #find_random_move(valid_moves)
-#     find_best_move_no_recursion(gs, valid_moves)
-#     print(counter)
-#     return_queue.put(next_move)
-
 def find_best_move(gs, valid_moves, return_queue):
     global next_move, counter
     counter = 0
